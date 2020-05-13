68b4ecd3fe1b11cf44ab47d14969f9c1a01d6c50
Szymon <wawakszymon@gmail.com>
2020-05-13 16:45:01 +0200

    Initial commit

---
+++ src/main/java/pl/bebeauty/BebeautyApplication.java
@@ -0,0 +1,13 @@
+package pl.bebeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebeautyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BebeautyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bebeauty/BebeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bebeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BebeautyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
